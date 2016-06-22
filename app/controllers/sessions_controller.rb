408169f4d52d1df4b33ac14ce2293953be2b60bf
Tana Jukes <tana.jukes@gmail.com>
2016-06-22 16:21:54 -0700

    Add login/logout links
    Add footer to main layout file so that links are always visible. Add sessions route for 'destroy' to clear the current session. Add #destroy action in sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,4 +12,10 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to movies_path, notice: "Ciao!"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'users/new'
-
-  get 'users/create'
-
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
