b05131be8e1c8386faa169088ecdde4be99a02ce
Tana Jukes <tana.jukes@gmail.com>
2016-06-22 20:05:05 -0700

    Helper method to show user's full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
    validates :lastname, presence: true
    validates :password, length: { in: 6..20 }, on: :create
 
+   def full_name
+     "#{firstname} #{lastname}"
+   end
 end
