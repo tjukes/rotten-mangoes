41eb329691b1e42d91b0926a35854b9f6d128fd4
Tana Jukes <tana.jukes@gmail.com>
2016-06-22 19:47:23 -0700

    Get review averages for movies
    Add method to Movie model to get the average of all of a movie's reviews. Returns nil if there are no reviews. This is an unprotected method, so it can be accessed directly from within views.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,6 +15,14 @@ class Movie < ActiveRecord::Base
 
   validate :release_date_is_in_the_past
 
+  def review_average
+    if reviews.empty?
+      nil
+    else
+      reviews.sum(:rating_out_of_ten)/reviews.size
+    end
+  end
+
   protected
   def release_date_is_in_the_past
     if release_date.present?
