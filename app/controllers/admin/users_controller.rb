89941ce911f84e8fdcb29c596d064a1ef145135b
Tana Jukes <tana.jukes@gmail.com>
2016-06-23 16:43:14 -0700

    Foundation for Admins
    Add namespaces controller, routes, and starter assets for Admins. Admins will be Users with special privileges for managing other users.

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,29 @@
+class Admin::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path, notice: "#{@user.firstname} #{@user.lastname}'s account was submitted successfully!"
+    else
+      render :new
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
