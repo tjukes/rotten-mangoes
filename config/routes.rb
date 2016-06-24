2bfe46524f0d8402a827142649798c508922d66d
Tana Jukes <tana.jukes@gmail.com>
2016-06-24 13:19:19 -0700

    Route /search requests to /movies/index
    Search queries (GET request) route to the movies#index action rather than displaying a search results page, because the view and basic functionality of those pages would be virtually identical.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
   end
+
+  get '/movies/search', to: 'movies#index'
+  
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
+
+
   resources :users, only: [:new, :create]
+
   resources :sessions, only: [:new, :create, :destroy]
+
   root to: 'movies#index'
 end
