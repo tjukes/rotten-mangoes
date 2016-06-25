a71107e02ac319daeae41c74fb23d472f07b41c3
Tana Jukes <tana.jukes@gmail.com>
2016-06-25 16:59:39 -0700

    Add functionality for admins to update users
    Admins should be able to edit/update user accounts, not just view and delete them. This commit defines the 'update' action in the admin users_controller, including a protected method to take in the appropriate params from an update form.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,6 +17,16 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path(@user), notice: "Account saved"
+    else
+      render :edit
+    end
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -33,4 +43,10 @@ class Admin::UsersController < ApplicationController
     redirect_to admin_users_path, notice: "#{@user.firstname} #{@user.lastname}'s account was successfully deleted."
   end
 
+  protected
+  def user_params
+    params.require(:user).permit(
+      :firstname, :lastname, :email, :password
+    )
+  end
 end
