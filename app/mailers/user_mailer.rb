6c120279f0dd4e877d77d8cc52ceb46acbae21b9
Tana Jukes <tana.jukes@gmail.com>
2016-06-25 18:07:53 -0700

    Define method for sending goodbye email
    Set standard settings for sending a goodbye email (confirmation of account deletion). The method takes a user as an argument. It assumes that the user has an 'email' attribute, and calls the mail() function to send a message to that email address with a default subject line - set here as 'Account deleted'.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: 'fruits@rottenmangoes.com'
+
+  def goodbye_email(user)
+    @user = user
+    mail(to: @user.email, subject: "Account deleted")
+  end
+
 end
